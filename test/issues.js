16771ccd4361bf78c3bf53ba754f36d229701780
Sean Lang <slang800@gmail.com>
2015-11-15 20:36:38 -0500

    add test case for nested objects

--- test/issues.js
+++ test/issues.js
@@ -17,3 +17,17 @@ test('#66', function (t) {
   stream.write('["foo":bar[');
   stream.end();
 });
+
+test('#81 - failure to parse nested objects', function (t) {
+  var stream = JSONStream
+    .parse('.bar.foo')
+    .on('error', function (err) {
+      t.error(err);
+    })
+    .on('end', function () {
+      t.end();
+    });
+
+  stream.write('{"bar":{"foo":"baz"}}');
+  stream.end();
+});
